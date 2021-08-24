f0b5138fed3265515d4d1ea375f07caa31c27703
Tyler Williams <tyler.williams1010@outlook.com>
2021-08-23 20:37:03 -0400

    Update WUMBO.py
    Proper Wumbology

--- WUMBO.py
+++ WUMBO.py
@@ -1,125 +1,159 @@
 # [WUMBO]
-# Weighted UID-filtered anoMaly-Based Outlier detection
+# Weighted UID-filtered Multi-Metric Based Outlier detection
 import numpy as np
-import pandas as pd
+from sklearn.neighbors import KernelDensity
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import BallTree
-
-def calc_dist_and_density(nn_arr, curr_array, kn):
-  # Calculate Average Distance for k-NN
-  tree = BallTree(nn_arr)
-  dist, ind = tree.query(curr_array,k=kn)
-  avg_dist = np.mean(dist)
-
-  # Calculate Inverse Density using Silverman's Rule of Thumb for Bandwidth
-  h = 0.9 * nn_arr.std() * len(nn_arr) ** (-1/5)
-  density = tree.kernel_density(X=curr_array, h=h)
-  density_val = 1/(density[0])
-  return avg_dist, density_val
-
-def filter_uid(df, uid_field, curr_uid):
-  deduped_array = df[df[uid_field]!=curr_uid].drop([uid_field, "weight", "avg_dist", "Total_Z"], axis=1).to_numpy()
-  return deduped_array
-
-def robust_scaler_Z(df, col, alpha):
-  min_perc = df[col].quantile(alpha)
-  max_perc = df[col].quantile(1 - alpha)
-
-  temp_df = df.loc[(df[col] > min_perc) & (df[col] < max_perc)]
-  avg = temp_df[col].mean()
-  std = temp_df[col].std()
-
-  df["avg"] = avg
-  df["std"] = std
-
-  df["Z_" + col] = (df[col] - df["avg"]) / df["std"]
-  df = df.drop(["avg", "std"], axis=1)
-
-  return df
-
-def convert_alpha(alpha):
-  # Turn alpha value into Z-Score based on Chebyshev's Inequality
-  # for a one-tailed test
-  Z = ((alpha * 2) ** -1) ** 0.5 # invert and take square root
-  return Z
-
-def wumbo(dataframe, uid_colname, alpha=0.01):
-  # [WUMBO] - Weighted UID-filtered anoMaly-Based Outlier detection 
-  ##################################################################
-  # Wumbo is an anomaly detector designed for large datasets.
-  # The algorithm requires no hyperparameters to be chosen except
-  # alpha - starting by filtering out similar identities so that
-  # one identity can't cluster with itself. Additionally, the
-  # primary measurements are kernel density and average distance
-  # from its k-Nearest Neighbors. These metrics are then combined
-  # and evaluated to determine Outlierness.
-  ##################################################################
+import pandas as pd
 
 
 
-  # Initialize a temporary and return dataframe 
-  temp_df = dataframe.copy()
-  
-  # Calculate number of k-Neighbors 
-  ##############################################################################
-  # This is a number that is equal to the square root of distinct count of UID's
-  # with a minimum of 5 and a maximum of 50.
-  # The number of k-Neighbors scales with the size of the data
-  ##############################################################################
+class Wumbo:
 
-  kn = min(max(int(len(dataframe[uid_colname].unique()) ** 0.5),5),50)
+    def __init__ (self,dataframe, uidColumnName, filterValue=True, alpha=0.01, features=["avgDistance", "maxDistance", "localDensity"]):
+        self.dataframe = dataframe
+        self.uidColumnName = uidColumnName
+        self.filter = filterValue
+        self.alpha = alpha 
+        self.features = features
 
-  # Calculate Z
-  Z = convert_alpha(alpha)
-
-  # Initialize additional columns
-  temp_df["weight"] = 0
-  temp_df["avg_dist"] = 0
-  temp_df["Total_Z"] = 0
+    
+    def calculateFeatures(self, distancesArray, nearestNeighborsArray, iterVector) -> dict:
+        
+        resultsDict = {}
 
 
-  # Iterate through dataframe
-  for x in range(len(temp_df.index)):
-    # Identify current UID Value
-    curr_uid = temp_df.loc[x,uid_colname]
+        if "avgDistance" in self.features:
+            # Calculate Average Distance for k-Nearest Neighbos
+            resultsDict["avgDistance"] = np.mean(distancesArray)
 
-    # Filter out UID's and convert to numpy
-    deduped_array = filter_uid(df=temp_df, uid_field=uid_colname,curr_uid=curr_uid)
+        if "maxDistance" in self.features:
+            # Calculate Max Distance for k-th Neighbor
+            resultsDict["maxDistance"] = np.max(distancesArray)
 
-    # Identify current array and convert to numpy
-    curr_array = temp_df.reset_index()
-    curr_array = curr_array.loc[curr_array["index"]==x].drop(["index", uid_colname, "weight", "avg_dist", "Total_Z"],axis=1).to_numpy()
+        if "localDensity" in self.features:
+            kde = KernelDensity(kernel='gaussian', bandwidth=1.0).fit(nearestNeighborsArray)
+            
+            resultsDict["localDensity"] = -1 * kde.score(iterVector)
 
-    # Find nearest neighbors
-    neigh = NearestNeighbors(n_neighbors=kn)
-    neigh.fit(deduped_array)
-    arr = [x for x in neigh.kneighbors(curr_array)[1][0][:]]
-    nn_arr = deduped_array[arr,:]
+        
+        return resultsDict
 
-    # Update avg_dist/inverse_density for row
-    avg_dist, inverse_density = calc_dist_and_density(nn_arr=nn_arr, curr_array=curr_array, kn=kn)
-    temp_df.loc[x, "avg_dist"] = avg_dist
-    temp_df.loc[x, "weight"] = inverse_density
+    def filterUID(self):
+        deduped_df = self.dataframe[self.dataframe[self.uidColumnName]!=self.currentUID].drop([self.uidColumnName], axis=1)
+    
+        return deduped_df
 
-  # Scale and calculate Z for weight/avg_dist
-  temp_df = robust_scaler_Z(temp_df, col="weight", alpha=alpha)
-  temp_df = robust_scaler_Z(temp_df, col="avg_dist", alpha=alpha)
+    def convertAlpha(self):
+        # Convert alpha to "Risk Threshold", which is just alpha inverted
+        riskThreshold = 1/self.alpha 
+        return riskThreshold
 
-  # Transform Z_* fields to remove negatives while removing a multiply by zero issue
-  temp_df["Z_weight"].apply(lambda x: max(0, x) + 1)
-  temp_df["Z_avg_dist"].apply(lambda x: max(0, x) + 1)
-  
-  # Calculate Weighted Outlier Score
-  temp_df["weighted_outlier_score"] = temp_df["Z_weight"] * temp_df["Z_avg_dist"]
+    def apply(self):
+        # [WUMBO] - Weighted UID-filtered Multi-Metric Based Outlier detection 
+        ##############################################################################
+        # Wumbo is an anomaly detector designed for large datasets with pockets of
+        # common groups.
+        #
+        # Part of the output of Wumbo is the Outlier Score [0,1], the other part
+        # is the weight, or Risk Score, a number that represents "Outlierness"
+        # of the node.
+        #
+        # The algorithm requires no hyperparameters to be chosen except
+        # alpha. 
+        #
+        # There are some optional arguments to select starting with filtering out similar
+        # identities/uid's so that one identity/uid can't cluster with itself to poison 
+        # the density values. 
+        #
+        # Additionally, the default measurements are kernel density, average distance, 
+        # and max distance from some k-Nearest Neighbors where k is already
+        # calculated by 5 <= sqrt(N) <= 50. This helps scale the dataset to a large
+        # number of data points with common behavioral characteristics relative to the
+        # size of the total, larger dataset.
+        # 
+        # These metrics are then combined and evaluated against the entire dataset
+        # to find outliers and Score the weighted "Risk."
+        ##############################################################################
 
-  # Transform weighted_outlier_score with Robust Scaler Z calculation
-  temp_df["Total_Z"] = robust_scaler_Z(temp_df, col="weighted_outlier_score", alpha=alpha)
 
-  # Initialize Outlier column, determine label via Z calculated before
-  temp_df["Outlier"] = 0
-  temp_df["Outlier"] = np.where(temp_df["weighted_outlier_score"]>=Z * 2, 1, 0) # Multiplying Z by 2 to solve for bounding issue with the earlier lambda transform
+        
+        # Initialize a temporary and return dataframe 
+        temp_df = self.dataframe.copy(deep=True)
+        results_df = pd.DataFrame()
+        
+        # Calculate number of k-Neighbors 
+        ##############################################################################
+        # This is a number that is equal to the square root of distinct count of UID's
+        # with a minimum of 5 and a maximum of 50. (Concept comes from t-SNE paper)
+        # This way the number of k-Neighbors scales with the size of the data.
+        ##############################################################################
 
-  # Return df with only original columns, Z, and Outlier
-  temp_df = temp_df.drop(["weight", "avg_dist", "Total_Z", "Z_weight", "Z_avg_dist", "weighted_outlier_score", "avg", "std"], axis=1)
 
-  return temp_df
+        kNeighbors = min(max(int(len(self.dataframe[self.uidColumnName].unique()) ** 0.5),5),50)
+        numberOfRows = len(self.dataframe.index)
+    
+    
+        # Initialize feature columns
+        if "avgDistance" in self.features:
+            results_df["avgDistance"] = 0
+        if "maxDistance" in self.features:
+            results_df["maxDistance"] = 0
+        if "localDensity" in self.features:
+            results_df["localDensity"] = 0
+
+        
+        # Iterate through dataframe
+        for x in range(numberOfRows):
+            
+            # Identify current UID Value
+            iterVector: np.array
+            
+            # Filter (or not)
+            if self.filter==True: 
+                temp_df = self.dataframe.copy(deep=True)
+                currentUID = temp_df.loc[x,self.uidColumnName]
+
+                # Filter out UID's and convert to numpy
+                iterVector = temp_df.loc[x].drop(self.uidColumnName).reset_index(drop=True).to_numpy().reshape(1,-1)
+                neighbors = NearestNeighbors(n_neighbors=kNeighbors)
+                neighbors.fit(temp_df.loc[x | temp_df[self.uidColumnName]!=currentUID].drop([self.uidColumnName], axis=1).to_numpy())
+                distancesArray, ind = neighbors.kneighbors(iterVector, return_distance=True)
+                
+                nearestNeighborsArray = temp_df[temp_df.index.isin(ind[0])].drop(labels=self.uidColumnName,axis=1).to_numpy()
+
+            else:
+                currentUID = temp_df.loc[x,self.uidColumnName]
+                #print(len(temp_df))
+                # Find nearest neighbors
+                iterVector = temp_df.loc[x].drop(self.uidColumnName).reset_index(drop=True).to_numpy().reshape(1,-1)
+                neighbors = NearestNeighbors(n_neighbors=kNeighbors)
+                neighbors.fit(temp_df.drop([self.uidColumnName], axis=1).to_numpy())
+                distancesArray, ind = neighbors.kneighbors(iterVector, return_distance=True)
+                
+                nearestNeighborsArray = temp_df[temp_df.index.isin(ind[0])].drop(labels=self.uidColumnName,axis=1).to_numpy()
+            
+            
+            
+            # Calculate Features (for layer 1) based on Feature Values for Model
+            resultsDict = self.calculateFeatures(distancesArray=distancesArray, nearestNeighborsArray=nearestNeighborsArray, iterVector=iterVector)
+
+            resultsDict[self.uidColumnName] = currentUID
+            results_df = results_df.append(resultsDict, ignore_index=True)
+            
+
+
+        output_df = self.dataframe.copy(deep=True)
+        kde = KernelDensity(kernel='gaussian', bandwidth=1.0).fit(results_df[self.features].to_numpy())
+
+        for x in range(numberOfRows):
+
+            iterRow = results_df.loc[x].drop("uid").reset_index(drop=True).to_numpy().reshape(1,-1)    
+                
+            output_df.loc[x, "Risk Score"] = 1/(numberOfRows * 10 ** (kde.score(iterRow)))
+            if 1/((numberOfRows * 10 ** (kde.score(iterRow)))) > (1/self.alpha):
+                output_df.loc[x, "Outlier"] = 1
+            else:
+                output_df.loc[x, "Outlier"] = 0
+        
+        return output_df
